0f8e4588ecec2473f076998ebd557ecae609cc1a
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-12 08:43:19 +0200

    importamos libreria

---
+++ examen.py
@@ -0,0 +1,4 @@
+#En primer lugar vamos a exportar librerias que vamos a usar para hacer el código
+
+from numpy.lib.function_base import append #Esto lo vamos a usar para agregar elementos a una lista
+import pandas as pd 
